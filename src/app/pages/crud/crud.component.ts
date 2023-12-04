7110958236bb157bd9b500b0d820d21dbac1f4d9
Santiago Nivia <millernivia1301@gmail.com>
2023-12-03 23:42:20 -0500

    Formularios y conexión al servicio

--- src/app/pages/crud/crud.component.ts
+++ src/app/pages/crud/crud.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { firstValueFrom } from 'rxjs';
+import { UserService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-crud',
@@ -7,9 +10,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrudComponent implements OnInit {
 
-  constructor(  ) {
+  users!: any[]
+  showModal: boolean = false;
+  actionUser: 'crear' | 'eliminar' | 'editar' | '' = '';
+  userActive!: string;
+  uidUserMongo: string = '';
+  formUser!: FormGroup;
+
+  constructor( private userService: UserService, private formBuilder: FormBuilder ) {
+    this.initFormUser();
   }
 
   ngOnInit(): void {
+    this.getAllUsers();
   }
+
+  initFormUser() {
+    this.formUser = this.formBuilder.group({
+      name: [''],
+      img: [''],
+      role: [''],
+      status: [''],
+      idUser: [''],
+      email: [''],
+      phone: [''],
+    });
+  }
+
+  async getAllUsers() {
+    this.users = await firstValueFrom( this.userService.getAllUsers() );
+  }
+
+  async getUser(id: string) {
+    const user = await firstValueFrom( this.userService.getUser(id) );
+    this.formUser.patchValue(user[0]);
+    this.uidUserMongo = user[0]._id;
+    return user;
+  }
+
+  async createUser() {
+    const createdUser = await firstValueFrom( this.userService.createUser(this.formUser.value) );
+    this.showModal = false;
+    this.getAllUsers();
+    this.formUser.reset();
+    return createdUser;
+  }
+
+  async updateUser(id: string) {
+    const updateUser = await firstValueFrom( this.userService.updateUser(this.uidUserMongo, this.formUser.value) );
+    this.getAllUsers();
+    this.formUser.reset();
+    this.showModal = false;
+    return updateUser;
+  }
+
+  async deleteUser(id: string) {
+    const deleteUser = await firstValueFrom( this.userService.deleteUser(this.uidUserMongo) );
+    this.getAllUsers();
+    this.showModal = false;
+    return deleteUser;
+  }
+
 }
